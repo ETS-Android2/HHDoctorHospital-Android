6c977741676f2ccee3066c875661ce364a70fa2c
thechildrenandroid <17864213635@163.com>
2022-02-23 12:12:35 +0800

    update

--- app/src/main/java/com/chuangjin/yunyisdk/activity/LauncherActivity.java
+++ app/src/main/java/com/chuangjin/yunyisdk/activity/LauncherActivity.java
@@ -7,12 +7,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 import com.chuangjin.HHDoctorV.HHDoctorSDK;
 import com.chuangjin.HHDoctorV.WebViewShopXActivity;
 import com.chuangjin.yunyisdk.R;
 
 import org.jetbrains.annotations.Nullable;
+
+import mlxy.utils.T;
 
 /**
  * Created by cxf on 2018/9/17.
@@ -26,20 +29,13 @@ public class LauncherActivity extends Activity {
         findViewById(R.id.watch).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Toast.makeText(LauncherActivity.this,"请使用手表Demo版本SDK", Toast.LENGTH_LONG).show();
             }
         });
         findViewById(R.id.hospital).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                HHDoctorSDK.openHospital(LauncherActivity.this);
-                //机构号、包名
-                //根据接口响应值跳转
-                //WebViewShopXActivity.forwardWebAllURL(LauncherActivity.this, url);
-                String phone="17864213635";
-                String url="https://his.drant.net/hospital-member/?phone="+phone+"&key=11&type=myyy";
-                Intent intent = new Intent(LauncherActivity.this, WebViewShopXActivity.class);
-                intent.putExtra("url", url);
-                startActivity(intent);
+                HHDoctorSDK.openHospital(LauncherActivity.this,"17864213635");
             }
         });
     }
